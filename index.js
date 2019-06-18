6cf67a45aa01465ebb372545ae06566d0a09da40
Александр Морозов <iamaleks.morozov@gmail.com>
2019-06-18 18:19:12 +0300

    init

--- index.js
+++ index.js
@@ -8,39 +8,59 @@ var token = '842396489:AAFpo0eafA4SEtJ7hv4ECqwzCY0eH2N4FRM';
 // Включить опрос сервера
 var bot = new TelegramBot(token, { polling: true });
 
+var answerUser = [];
+
 var countRightAnswer = 0;
 var numberAnswer = 0;
 
 var questions = [
   {
-    title: 'Первый вопрос. Сколько параметров можно передать функции ?',
+    title: 'Вашему аккаунту больше года?',
     buttons: [
-      [{ text: 'Ровно столько, сколько указано в определении функции.', callback_data: '0_1' }],
-      [{ text: 'Сколько указано в определении функции или меньше.', callback_data: '0_2' }],
-      [{ text: 'Сколько указано в определении функции или больше.', callback_data: '0_3' }],
-      [{ text: 'Любое количество.', callback_data: '0_4' }]
+      [{ text: 'Да', callback_data: '0_1' }],
+      [{ text: 'Нет', callback_data: '0_2' }],
     ],
-    right_answer: 4
+    right_answer: 1
   },
   {
-    title: 'Второй вопрос. Чему равна переменная name?\nvar name = "пупкин".replace("п", "д")',
+    title: 'К Вашему аккаунту привязана действующая почта и мобильный телефон?',
     buttons: [
-      [{ text: 'дудкин', callback_data: '1_1' }],
-      [{ text: 'дупкин', callback_data: '1_2' }],
-      [{ text: 'пупкин', callback_data: '1_3' }],
-      [{ text: 'ляпкин-тяпкин', callback_data: '1_4' }]
+      [{ text: 'Да', callback_data: '1_1' }],
+      [{ text: 'Нет', callback_data: '1_2' }],
     ],
-    right_answer: 2
+    right_answer: 1
   },
   {
-    title: 'Третий вопрос. Чему равно 0 || "" || 2 || true ?',
+    title: 'Ваш аккаунт заполнен (содержит информацию об учебе или работе, есть фото, друзья, записи на стене)?',
     buttons: [
-      [{ text: '0', callback_data: '2_1' }],
-      [{ text: '""', callback_data: '2_2' }],
-      [{ text: '2', callback_data: '2_3' }],
-      [{ text: 'true', callback_data: '2_4' }]
+      [{ text: 'Да', callback_data: '2_1' }],
+      [{ text: 'Нет', callback_data: '2_2' }],
     ],
-    right_answer: 3
+    right_answer: 1
+  },
+  {
+    title: 'Вы проявляли социальную активность на аккаунте в течении всего его существования?',
+    buttons: [
+      [{ text: 'Да', callback_data: '3_1' }],
+      [{ text: 'Нет', callback_data: '3_2' }],
+    ],
+    right_answer: 1
+  },
+  {
+    title: 'Есть ли у Вас возможность проявлять социальную активность  с мобильного устройства во время аренды аккаунта?',
+    buttons: [
+      [{ text: 'Да', callback_data: '4_1' }],
+      [{ text: 'Нет', callback_data: '4_2' }],
+    ],
+    right_answer: 1
+  },
+  {
+    title: 'Согласны ли Вы на изменение пароля во время аренды аккаунта? (Это делается по техническим причинам, так как Facebook периодически просит о смене пароля для безопасности; новый пароль будет согласован с Вами)',
+    buttons: [
+      [{ text: 'Да', callback_data: '5_1' }],
+      [{ text: 'Нет', callback_data: '5_2' }],
+    ],
+    right_answer: 1
   },
 ];
 
@@ -61,20 +81,37 @@ function newQuestion(msg) {
 }
 
 bot.onText(/\/start/, function (msg, match) {
+  bot.sendMessage(msg.from.id, `Мы – команда молодых SMM-специалистов. Недавно мы начали развивать направление Facebook Ads и столкнулись с проблемой ограниченных рекламных возможностей в этой социальной сети. К сожалению, Facebook запрещает создавать рекламные объявления со свежих аккаунтов, а также ограничивает создание объявлений с одного аккаунта. В связи с этим мы проделываем следующие этапы:
+
+  1. арендуем аккаунты у людей 
+  2. используем рекламные возможности аккаунтов 
+       (1 неделя – 3+ месяца) 
+  3. возвращаем социальный аккаунт в целости и сохранности. 
+  
+Безопасно ли это? 
+Да! Мы подписываем с Вами договор при личной встрече в 2х экземплярах. У Вас всегда будет полноценный доступ к аккаунту. Никаких постов от вашего имени и спама – Ваши друзья не догадаются, что вы зарабатываете на своей страничке. Мы будем комментировать и делать репосты новостей с популярных СМИ, но эти посты никто кроме вас не будет видеть. Мы не будем изменять Ваш логин. Пароль может быть изменен, но после предупреждения и согласования нового с Вами. Это делается по техническим причинам, так как Facebook периодически просит о смене пароля для безопасности. 
+  
+Пройдите небольшой опрос, чтобы мы могли узнать больше о Вашем аккаунте: 
+  `);
   numberAnswer = 0;
   countRightAnswer = 0;
+  answerUser = [];
   newQuestion(msg);
 });
 
 bot.on('callback_query', function (msg) {
+  console.log(msg.from.id);
   var answer = msg.data.split('_');
   var index = answer[0];
   var button = answer[1];
 
+
   if (questions[index].right_answer == button) {
     countRightAnswer++;
+    answerUser.push(` ${++index} - Да`);
+  } else {
+    answerUser.push(` ${++index} - Нет:(`);
   }
-
   if (numberAnswer === questions.length) {
     endPoll(msg);
   }
@@ -82,9 +119,10 @@ bot.on('callback_query', function (msg) {
 });
 
 function endPoll(msg) {
-  if (countRightAnswer >= 2) {
-    bot.sendMessage(msg.from.id, "Отлично, вы прошли тест! Перейдите по ссылке123:  vk.com");
+  if (countRightAnswer === questions.length) {
+    bot.sendMessage(230431843, `Юзер который прошел опрос - @${msg.from.username}`);
+    bot.sendMessage(msg.from.id, "Спасибо, наш менеджер скоро с вами свяжется:)");
   } else {
-    bot.sendMessage(msg.from.id, "К сожалению вы нам не подходите, попробуйте в другой раз.")
+    bot.sendMessage(msg.from.id, "К сожалению вы нам не подходите, попробуйте в следующий раз.")
   }
 }
